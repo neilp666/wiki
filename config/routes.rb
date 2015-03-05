310de6227f4fae92bd1e30d0b89acf48f18fe6e0
neil patel <neil@myindianweddingapp.com>
2015-03-05 12:31:55 +0000

    Add devise and generate user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :articles
   root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
