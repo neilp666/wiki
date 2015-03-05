6b32682dede825e3165cb071718e305fc8a92baa
neil patel <neil@myindianweddingapp.com>
2015-03-05 11:29:14 +0000

    Loop articles on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
 
-	def method
+	def index
+		@articles = Article.all.order("created_at DESC")
+
 	end
 
 	def new
 		@article = Article.new
+	end
+
+	def show
 	end
 
 	def create
@@ -17,6 +23,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :content)
